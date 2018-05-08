b3c0d3304835b0cede30784ae605e4b7606ea8f2
geng <snggeng@gmail.com>
2018-05-08 10:11:26 -0400

    update: package.json | add proxy; signup is connected with redux and sagas

--- client/src/signup/reducer.js
+++ client/src/signup/reducer.js
@@ -1,25 +1,58 @@
-import { SIGNUP_REQUESTING } from './constants'
-
+import {  
+    SIGNUP_REQUESTING,
+    SIGNUP_SUCCESS,
+    SIGNUP_ERROR,
+} from './constants'
+  
 const initialState = {  
-  requesting: false,
-  successful: false,
-  messages: [],
-  errors: [],
+    requesting: false,
+    successful: false,
+    messages: [],
+    errors: [],
 }
-
+  
 const signupReducer = (state = initialState, action) => {  
-  switch (action.type) {
+switch (action.type) {
     case SIGNUP_REQUESTING:
-      return {
+    return {
         requesting: true,
         successful: false,
         messages: [{ body: 'Signing up...', time: new Date() }],
         errors: [],
-      }
+    }
+
+    // reset the state and add a body message of success!
+    // remember our successful returned payload will be:
+    // {"username": "of the new user", "id": "of the user"}
+    case SIGNUP_SUCCESS:
+    return {
+        errors: [],
+        messages: [{
+        body: `Successfully created account for ${action.response.username}`,
+        time: new Date(),
+        }],
+        requesting: false,
+        successful: true,
+    }
+
+    // reset the state but with errors!
+    // the error payload returned is actually far
+    // more detailed, but we'll just stick with
+    // the base message for now
+    case SIGNUP_ERROR:
+    return {
+        errors: state.errors.concat([{
+        body: action.error.toString(),
+        time: new Date(),
+        }]),
+        messages: [],
+        requesting: false,
+        successful: false,
+    }
 
     default:
-      return state
-  }
+    return state
+}
 }
 
-export default signupReducer
+export default signupReducer 
