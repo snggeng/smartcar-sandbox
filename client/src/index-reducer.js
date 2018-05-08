5e5edfb40c2cc35f3cc41505472773bfc2025dc1
geng <snggeng@gmail.com>
2018-05-08 10:50:34 -0400

    update: index-reducer.js | combine loginReducer

--- client/src/index-reducer.js
+++ client/src/index-reducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux'
 import { reducer as form } from 'redux-form'
 import user from './user/reducer'
 import signup from './signup/reducer'
+import login from './login/reducer'
 
 const IndexReducer = combineReducers({
+    login,
     signup,  
     user,
     form,
